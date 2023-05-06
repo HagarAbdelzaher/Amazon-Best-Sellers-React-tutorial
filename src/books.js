b4b7efb82715d333cc841c8a3a55f1e0ba1cf25a
HagarAbdelzaher <hagarr.abdelzaher@gmail.com>
2023-05-06 16:57:45 -0700

    first commit

---
+++ src/books.js
@@ -0,0 +1,21 @@
+export const books = [
+  {
+    id: 1,
+    title: " I Will Teach You to Be Rich",
+    author: "Ramit Sethi",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81Cm1OaHhIL._AC_UL600_SR600,400_.jpg",
+  },
+  {
+    id: 2,
+    title: "The body keeps the score",
+    author: "Bessel van der Kolk M.Dr",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71bVExi4yXL._AC_UL600_SR600,400_.jpg",
+  },
+  {
+    id: 3,
+    title: "The 48 Laws of Power",
+    author: "Robert Greene",
+
+    img: "https://images-na.ssl-images-amazon.com/images/I/71aG+xDKSYL._AC_UL600_SR600,400_.jpg",
+  },
+];
